c0f3cb95782740c8f0bba2bbfa2451c85a1c7e5c
anasa <anasa@LAPTOP-FPAKTMJC.lan>
2023-03-06 15:59:32 -0500

    Created two classes for Freshman and Senior and their default constructors

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author anasa
+ */
+public class Freshman extends Student {
+    
+    /**
+     * default constructor
+     */
+    public Freshman() {
+        super("", 0, 0);
+    }
+    
+}
